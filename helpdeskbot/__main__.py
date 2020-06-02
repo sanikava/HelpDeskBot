378e64c8615eca77acfa5c6b921792ec24d10b12
John Leon <nanodano@devdungeon.com>
2020-06-01 23:19:21 -0500

    Fixing environ get to avoid error

--- helpdeskbot/__main__.py
+++ helpdeskbot/__main__.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 def main():
     load_dotenv()
-    if not environ['DISCORD_CHANNEL'] or not environ['DISCORD_TOKEN']:
+    if not environ.get('DISCORD_CHANNEL') or not environ.get('DISCORD_TOKEN'):
         raise Exception('Missing DISORD_CHANNEL or DISCORD_TOKEN env var.')
     bot = HelpDeskBot(environ['DISCORD_CHANNEL'], environ['DISCORD_TOKEN'])
     bot.run()
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='helpdeskbot',
-    version='2.0.0',
+    version='2.0.1',
     description='Discord utility bot for DevDungeon.com',
     url='https://github.com/DevDungeon/HelpDeskBot',
     author='John Leon',
